cf25f0ea951c6fbf5ca341b5ef75f41f2cbd1035
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-10-30 03:33:46 +0700

    Create user-input-madlibs.py

---
+++ user-input-madlibs.py
@@ -0,0 +1,10 @@
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter a second adjective: ")
+adjective3 = input("Enter a third adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print(f"Today, I went to a {adjective1} zoo")
+print(f"In an exhibit, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
