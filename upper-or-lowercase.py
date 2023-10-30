daa6f514db216fe67adbd9f332caf620713f40ab
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-10-30 21:59:39 +0700

    Update upper-or-lowercase.py

---
+++ upper-or-lowercase.py
@@ -0,0 +1,8 @@
+text = input("What is your text: ")
+mode = input("How would you like to transform your text (upper/lower): ")
+
+if mode == "upper":
+    print(text.upper())
+
+elif mode == "lower":
+    print(text.lower())
