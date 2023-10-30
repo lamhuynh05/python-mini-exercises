27c826756a55c1480aaaee585dcbfa15e704ae94
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-10-30 22:17:03 +0700

    Create logical-operators-weather.py

---
+++ logical-operators-weather.py
@@ -0,0 +1,13 @@
+temp = 20
+sunny = False
+
+if temp <= 0 or temp >= 30:
+    print("The temperature is bad:(")
+
+else:
+    print("The temperature is good:)")
+
+if not sunny: 
+    print("The weather is sunny today")
+else:
+    print("The weather is not sunny today")
