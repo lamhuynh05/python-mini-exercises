f166a876ea1a6e8932782de5a9811474a5c731ad
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-10-30 20:02:39 +0700

    Create math-circumference-circle.py

---
+++ math-circumference-circle.py
@@ -0,0 +1,7 @@
+import math #import math module to use math.pi
+radius = float(input("Enter the radius of a circle: "))
+
+circumference = 2 * math.pi * radius
+
+print(f"The circumference is: {round(circumference, 2)} cm")
+# round to 2 decimal places 
