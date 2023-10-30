82652ebb20f4f0215c9e8df97dfa95929e8fde5c
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-10-30 20:08:37 +0700

    Revert "math and if else"
    This reverts commit cfb0cc2f0f06a69b2b4b2ce17286e6f2e95dae18.

--- if-else-1-food.py
+++
@@ -1,6 +0,0 @@
-response = input("Would you like some food (Y/N): ")
-
-if response == "Y":
-    print("Have some food!")
-else:
-    print("No food for you >.<")
--- math-area-of-circle.py
+++
@@ -1,4 +0,0 @@
-import math
-radius = float(input("Enter the radius of circle: "))
-area = math.pi * pow(radius, 2)
-print(f"The area of the circle is {round(area, 2)} cm^2")
