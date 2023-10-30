a08e576771d41ef6a0ddb41508bb865ee06bc98e
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-10-30 20:01:00 +0700

    Create user-input-2-area-calc.py

---
+++ user-input-2-area-calc.py
@@ -0,0 +1,7 @@
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+
+volume = height * width * length
+
+print(f"The volume is {volume} cm^3")
