60dd3032cd635129cfeb0b6758c2f29f2d165cc4
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-11-29 16:56:12 +0700

    Create walrus-operator.py

---
+++ walrus-operator.py
@@ -0,0 +1,12 @@
+# foods = list()
+# while True:
+#     food = input("What food do you like?: ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+foods = list()
+while food := input("What food do you like?: ") != "quit":
+    foods.append(food)
+
+
