71220a7898e0e03d73b812be4e387dd88f363daf
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-11-30 16:31:15 +0700

    Create higher-order-functions.py

---
+++ higher-order-functions.py
@@ -0,0 +1,22 @@
+# 1
+def loud(text):
+    return text.upper()
+
+def quiet(text):
+    return text.lower()
+
+def hello(func):
+    text = func("Hello")
+    print(text)
+
+hello(loud)
+hello(quiet)
+
+# 2
+def divisor(x):
+    def dividend(y):
+        return y / x
+    return dividend
+
+divide = divisor(2)
+print(divide(10))
